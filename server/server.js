f40a08a806d0bb91e1742d47256d463899d015da
Shashank Lakhera <lakherashashank70@gmail.com>
2026-06-08 12:56:30 +0530

    feat: complete socket.io configuration

--- server/server.js
+++ server/server.js
@@ -1,12 +1,18 @@
 import app from "./src/app.js";
 import dotenv from "dotenv"
 import connectDB from "./src/config/database.js";
+import http from 'http'
+import initializeSocketServer from "./src/config/socket.js";
 dotenv.config()
-
 connectDB();
+
 let port = process.env.PORT || 4000;
 
-app.listen(port, () => {
+let httpServer = http.createServer(app)
+
+initializeSocketServer(httpServer)
+
+httpServer.listen(port, () => {
     console.log(`Server is running at ${port}`);
     
 })
---
+++ server/src/config/socket.js
@@ -0,0 +1,61 @@
+import {Server} from 'socket.io'
+
+const initializeSocketServer = (httpServer) => {
+
+    const io = new Server(httpServer, {
+        cors:{
+            origin:"http://localhost:5173",
+            // credentials: true
+        }
+    })
+
+    io.on("connection", (socket) => {
+        console.log("new User connected : ",socket.id);
+        
+        // 1. SETUP: User logs in and joins their personal room
+        socket.on("setup", (userData) => {
+            socket.join(userData._id);
+            socket.emit("connected")
+            console.log("User joined personal room : ",userData._id);
+        })
+
+        // 2. JOIN CHAT: User clicks on a specific chat 
+        socket.on("join chat", (room) => {
+            socket.join(room);
+            console.log("User joined chat room : ", room);
+        })
+
+        // 3. TYPING TNDICATORS: Broadcast to the specific chat room
+        socket.on("typing", (room) => {
+            socket.in(room).emit("typing")
+        })
+        socket.on("stop typing", (room) => {
+            socket.in(room).emit("stop typing");
+        })
+
+        // 4. NEW MESSAGE: Fast reply 
+        socket.on("new message", (newMessageRecieved) => {
+            let chat = newMessageRecieved.chat;
+
+            if(!chat.users){
+                console.log("chat.users not defined");
+                return 
+            }
+
+            chat.users.forEach(user => {
+                if(user._id === newMessageRecieved.sender._id) return;
+
+                socket.in(user._id).emit("message recieved", newMessageRecieved)
+            });
+        })
+
+
+        socket.on("disconnect", () => {
+            console.log("User disconnected : ",socket.id);
+        })
+    })
+
+    return io
+}
+
+export default initializeSocketServer;
